b3b540319040dcd17a0d66afc6d67f30d72d9557
Joy Lal Chattaraj <joyc@qubole.com>
2019-12-20 23:06:07 +0530

    Add Kustomize Templates

---
+++ kustomize/logging/monitoring-fluentd-install-plugins.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+fluent-gem install fluent-plugin-s3
+fluent-gem install fluent-plugin-forest
+fluent-gem install fluent-plugin-rewrite-tag-filter
+fluent-gem install fluent-plugin-webhdfs
+exec /run.sh
